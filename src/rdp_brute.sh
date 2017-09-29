3ef2710a736432277281f61cfd125ada4258a864
GetDrive <getdrive@users.noreply.github.com>
2017-09-30 02:37:43 +0300

    Update rdp_brute.sh

--- src/rdp_brute.sh
+++ src/rdp_brute.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # NOTE: YOU ARE FREE TO COPY,MODIFY,REUSE THE SOURCE CODE FOR EDUCATIONAL PURPOSE ONLY.
+#ver=1.17
 clear
 ##########################################COLOR######################################
 colorbase="\E[0m"
